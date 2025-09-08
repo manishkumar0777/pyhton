3e6009f8b68156d7e7cd021d075cfc2242f51315
manishkumar0777 <manishkumar970034@gmail.com>
2025-09-08 15:36:22 +0530

    pyhton

---
+++ basics/02_datatypes.py
@@ -0,0 +1,34 @@
+# Numbers --------------------------------------------------------------------------
+
+# Int
+a = 33 
+
+# Float
+b = 12.5
+
+# Complex
+c = 3 + 4j
+
+print(type(a), a)
+print(type(b), b)
+print(type(c), c)
+
+
+# Strings --------------------------------------------------------------------------
+username = 'Manish'
+print(type(username), username)
+
+# string treated as array of characters
+print(username[1])
+
+# negative counting starts from the end
+print(username[-1])
+
+# print in between characters - below code will print form index - 1 to 3 not 4
+print(username[1:4]) 
+
+
+# username[0] = 2 will five the error as string is immuatable
+
+
+
